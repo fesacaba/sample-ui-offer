6d84d734d26dd01f789e848e7b3a99d81d940341
Felipe Barreto <felipe.barreto@omni.com.br>
2022-05-27 14:11:51 -0300

    fix config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 const mobileRoutes: Routes = [
+    {
+        path: '',
+        redirectTo: '/mobile',
+        pathMatch: 'full',
+    },
     {
         path: '',
         loadChildren: () => import('./mobile/mobile.module').then(m => m.MobileModule)
