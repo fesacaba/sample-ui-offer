50f9ab1c3e647da9242e2a357d7fe8fd1e4c6723
Felipe Barreto <felipe.barreto@omni.com.br>
2022-05-27 14:22:33 -0300

    fix

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const nomeApp = process.env.npm_package_name;
 const app = express();
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist/offer-ui'));
+app.use(express.static(__dirname + '/dist/omni-ib-offer-account-front'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/offer-ui/index.html'));
+res.sendFile(path.join(__dirname+'/dist/omni-ib-offer-account-front/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
